43165273c69953dd639a8e08fb680ff5a94c4085
faezeh72a <www.mahi.ahmadi72@gmail.com>
2021-08-13 13:42:10 +0430

    comming from PC

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.querySelector('.i-heart').addEventListener('mouseenter',oneClick);
+{
+function oneClick(e){
+document.getElementsByClassName('i-heart').style.backgroundColor = `rgb(81,8,0)`
+}
+}
