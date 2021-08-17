f6ec91bb48d64634e5934dc2b48ecb34162df144
faezeh72a <www.mahi.ahmadi72@gmail.com>
2021-08-17 14:45:40 +0430

    commiting from PC

--- js/app.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.i-heart').addEventListener('mouseenter',oneClick);
-{
-function oneClick(e){
-document.getElementsByClassName('i-heart').style.backgroundColor = `rgb(81,8,0)`
-}
-}
